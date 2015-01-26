f5dc5d0c97e11dee26969e95f163772e41c944bd
Bove <ribohe94@gmail.com>
2015-01-26 13:57:25 -0600

    Agregar clase Carta, conjunto carta y Clase direccion carta (WILL)

---
+++ Modelo/src/modelo/Carta.java
@@ -0,0 +1,40 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Carta implements Serializable{
+    
+    // Constructor
+
+    public Carta(String DIRECCION_CARTA, String tipo, int numero) {
+        this.DIRECCION_CARTA = DIRECCION_CARTA;
+        this.tipo = tipo;
+        this.numero = numero;
+    }
+    
+    // Metodos
+
+    public String getDIRECCION_CARTA() {
+        return DIRECCION_CARTA;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        return s.append(numero).append(" de ").append(tipo).toString();
+    }
+    
+    // Atributos
+    
+    private String DIRECCION_CARTA;
+    private String tipo;
+    private int numero;
+}
---
+++ Modelo/src/modelo/DireccionCarta.java
@@ -0,0 +1,68 @@
+package modelo;
+
+public class DireccionCarta {
+
+    // Corazones
+    public final static String DIRECCION_Acorazones = "/tarea/pkg8/img/Actions/ace_of_hearts.png";
+    public final static String DIRECCION_Doscorazones = "/tarea/pkg8/img/Actions/2_of_hearts.png";
+    public final static String DIRECCION_Trescorazones = "/tarea/pkg8/img/Actions/3_of_hearts.png";
+    public final static String DIRECCION_Cuatrocorazones = "/tarea/pkg8/img/Actions/4_of_hearts.png";
+    public final static String DIRECCION_Cincocorazones = "/tarea/pkg8/img/Actions/5_of_hearts.png";
+    public final static String DIRECCION_Seiscorazones = "/tarea/pkg8/img/Actions/6_of_hearts.png";
+    public final static String DIRECCION_Sietecorazones = "/tarea/pkg8/img/Actions/7_of_hearts.png";
+    public final static String DIRECCION_Ochocorazones = "/tarea/pkg8/img/Actions/8_of_hearts.png";
+    public final static String DIRECCION_Nuevecorazones = "/tarea/pkg8/img/Actions/9_of_hearts.png";
+    public final static String DIRECCION_Diezcorazones = "/tarea/pkg8/img/Actions/10_of_hearts.png";
+    public final static String DIRECCION_Oncecorazones = "/tarea/pkg8/img/Actions/jack_of_hearts.png";
+    public final static String DIRECCION_Docecorazones = "/tarea/pkg8/img/Actions/queen_of_hearts.png";
+    public final static String DIRECCION_Trececorazones = "/tarea/pkg8/img/Actions/king_of_hearts.png";
+
+    // Treboles
+    public final static String DIRECCION_Atrebol = "/tarea/pkg8/img/Actions/ace_of_clubs.png";
+    public final static String DIRECCION_Dostrebol = "/tarea/pkg8/img/Actions/2_of_clubs.png";
+    public final static String DIRECCION_Trestrebol = "/tarea/pkg8/img/Actions/3_of_clubs.png";
+    public final static String DIRECCION_Cuatrotrebol = "/tarea/pkg8/img/Actions/4_of_clubs.png";
+    public final static String DIRECCION_Cincotrebol = "/tarea/pkg8/img/Actions/5_of_clubs.png";
+    public final static String DIRECCION_Seistrebol = "/tarea/pkg8/img/Actions/6_of_clubs.png";
+    public final static String DIRECCION_Sietetrebol = "/tarea/pkg8/img/Actions/7_of_clubs.png";
+    public final static String DIRECCION_Ochotrebol = "/tarea/pkg8/img/Actions/8_of_clubs.png";
+    public final static String DIRECCION_Nuevetrebol = "/tarea/pkg8/img/Actions/9_of_clubs.png";
+    public final static String DIRECCION_Dieztrebol = "/tarea/pkg8/img/Actions/10_of_clubs.png";
+    public final static String DIRECCION_Oncetrebol = "/tarea/pkg8/img/Actions/jack_of_clubs.png";
+    public final static String DIRECCION_Docetrebol = "/tarea/pkg8/img/Actions/queen_of_clubs.png";
+    public final static String DIRECCION_Trecetrebol = "/tarea/pkg8/img/Actions/king_of_clubs.png";
+
+    //Diamantes
+    public final static String DIRECCION_Adiamantes = "/tarea/pkg8/img/Actions/ace_of_diamonds.png";
+    public final static String DIRECCION_Dosdiamantes = "/tarea/pkg8/img/Actions/2_of_diamonds.png";
+    public final static String DIRECCION_Tresdiamantes = "/tarea/pkg8/img/Actions/3_of_diamonds.png";
+    public final static String DIRECCION_Cuatrodiamantes = "/tarea/pkg8/img/Actions/4_of_diamonds.png";
+    public final static String DIRECCION_Cincodiamantes = "/tarea/pkg8/img/Actions/5_of_diamonds.png";
+    public final static String DIRECCION_Seisdiamantes = "/tarea/pkg8/img/Actions/6_of_diamonds.png";
+    public final static String DIRECCION_Sietediamantes = "/tarea/pkg8/img/Actions/7_of_diamonds.png";
+    public final static String DIRECCION_Ochodiamantes = "/tarea/pkg8/img/Actions/8_of_diamonds.png";
+    public final static String DIRECCION_Nuevediamantes = "/tarea/pkg8/img/Actions/9_of_diamonds.png";
+    public final static String DIRECCION_Diezdiamantes = "/tarea/pkg8/img/Actions/10_of_diamonds.png";
+    public final static String DIRECCION_Oncediamantes = "/tarea/pkg8/img/Actions/jack_of_diamonds.png";
+    public final static String DIRECCION_Docediamantes = "/tarea/pkg8/img/Actions/queen_of_diamonds.png";
+    public final static String DIRECCION_Trecediamantes = "/tarea/pkg8/img/Actions/king_of_diamonds.png";
+
+    // Bastos
+    public final static String DIRECCION_Abastos = "/tarea/pkg8/img/Actions/ace_of_spades.png";
+    public final static String DIRECCION_Dosbastos = "/tarea/pkg8/img/Actions/2_of_spades.png";
+    public final static String DIRECCION_Tresbastos = "/tarea/pkg8/img/Actions/3_of_spades.png";
+    public final static String DIRECCION_Cuatrobastos = "/tarea/pkg8/img/Actions/4_of_spades.png";
+    public final static String DIRECCION_Cincobastos = "/tarea/pkg8/img/Actions/5_of_spades.png";
+    public final static String DIRECCION_Seisbastos = "/tarea/pkg8/img/Actions/6_of_spades.png";
+    public final static String DIRECCION_Sietebastos = "/tarea/pkg8/img/Actions/7_of_spades.png";
+    public final static String DIRECCION_Ochobastos = "/tarea/pkg8/img/Actions/8_of_spades.png";
+    public final static String DIRECCION_Nuevebastos = "/tarea/pkg8/img/Actions/9_of_spades.png";
+    public final static String DIRECCION_Diezbastos = "/tarea/pkg8/img/Actions/10_of_spades.png";
+    public final static String DIRECCION_Oncebastos = "/tarea/pkg8/img/Actions/jack_of_spades.png";
+    public final static String DIRECCION_Docebastos = "/tarea/pkg8/img/Actions/queen_of_spades.png";
+    public final static String DIRECCION_Trecebastos = "/tarea/pkg8/img/Actions/king_of_spades.png";
+    
+    // Imagen de Atrás
+    public final static String DIRECCION_ATRAS = "/cliente/imagenes/Trasera.png";
+
+}
