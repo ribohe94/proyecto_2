06d443b191ef6d5ae76e6398546dafefc0df91f5
Bove <ribohe94@gmail.com>
2015-02-01 15:59:05 -0600

    Ultimo commit

--- Cliente/src/cliente/VentanaPrincipal.java
+++ Cliente/src/cliente/VentanaPrincipal.java
@@ -1,11 +1,5 @@
 package cliente;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import modelo.GestorBD;
-import modelo.GestorJugador;
-import modelo.Jugador;
-import vista.VentanaCliente;
 import vista.VentanaRegistro;
 
 public class VentanaPrincipal {
@@ -14,19 +8,5 @@ public class VentanaPrincipal {
 
         VentanaRegistro aplicacion = new VentanaRegistro();
         aplicacion.mostrar();
-//        Jugador j1 = new Jugador("4", "Bove", "123");
-//        Jugador j2 = new Jugador("5", "Riccardo", "425");
-//
-//        GestorJugador gj = new GestorJugador();
-        try {
-//            gj.guardar(j1);
-//            gj.consultar();
-//        VentanaCliente vc = new VentanaCliente(null);
-//        vc.iniciar();
-        } catch (Exception ex) {
-            Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 }
-
-// Actualizacion will lista!!
