eedec0b522a2418d5f3cc2b782fe96c986f1e256
Bove <ribohe94@gmail.com>
2015-02-01 18:22:41 -0600

    Ultimo commit De verdad

--- Modelo/src/modelo/GestorBD.java
+++ Modelo/src/modelo/GestorBD.java
@@ -68,7 +68,7 @@ public class GestorBD {
     private static String URL = "//localhost:3306/";
     private static String BASE_DATOS = "modelopersona";
     private static String USUARIO = "root";
-    private static String CLAVE = "Belsavis1994";
+    private static String CLAVE = "root";
     private Connection cnx = null;
     String protocolo;
     String url;
